97e87188c7655fae9c3da0d719c1002c4235b2d9
Maxence Rougé <rouge.maxence31@gmail.com>
2021-09-21 14:40:37 +0200

    Update Difficulties.py

--- Difficulties.py
+++ Difficulties.py
@@ -1,4 +1,5 @@
 import pygame
+import random2
 
 easy = []
 medium = []
@@ -6,17 +7,32 @@ hard = []
 
 #Fonction de sélection de difficulté et initialisation du niveau
 def difficulties(difficulty):
-    if difficuly == "easy":
-        for i in range(5):
+    #Difficultée facile
+    if(difficulty == "easy"):
+        for i in range(20):
             easy.append([])
-            for y in range(5):
+            for y in range(20):
                 easy[i].append(0)
         return easy
 
+    #Difficultée moyenne
+    if(difficulty == "medium"):
+        for i in range(20):
+            medium.append([])
+            for y in range(20):
+                if (random2.randint(1,100) > 5):
+                    medium[i].append(0)
+                else:
+                    medium[i].append(1)
+        return medium
 
-        #medium.append = []
-        #print(medium)
-        #hard = []
-
-difficulties(1)
-print(easy)
+    #Difficultée difficile
+    if(difficulty == "hard"):
+        for i in range(20):
+            hard.append([])
+            for y in range(20):
+                if (random2.randint(1,100) > 15):
+                    hard[i].append(0)
+                else:
+                    hard[i].append(1)
+        return hard
